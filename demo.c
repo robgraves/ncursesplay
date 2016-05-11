2107f3009a8b796f4c1cdd07c2e6d4b0028c236e
robgraves <matthewpage2003@yahoo.com>
2016-05-10 22:07:26 -0400

    Now ball has collision detection and bounces back and forth between the window edges

--- demo.c
+++ demo.c
@@ -9,12 +9,19 @@
 
 int main()
 {
-    int x = 0;
-    int y = 0;
+    int x           = 0;
+    int y           = 0;
+    int max_x       = 0;
+    int max_y       = 0;
+    int next_x      = 0;
+    int direction   = 1;
 
     initscr();              //Initialize the window
     noecho();               //Don't echo any keypresses
     curs_set(FALSE);        //Don't display a cursor
+
+    //Global var 'stdscr' is created by the call to 'initscr()'
+    getmaxyx(stdscr, max_y, max_x);
 
     while(1)
     {
@@ -23,7 +30,17 @@ int main()
         refresh();
 
         usleep(DELAY);      //Shorter delay between movements
-        x++;                //Advance the ball to the right   
+
+        next_x = x + direction;
+
+        if (next_x >= max_x || next_x < 0)
+        {
+            direction *= -1;
+        }
+        else
+        {
+            x += direction;
+        }
     }
 
     endwin();               //Restore normal terminal behavior

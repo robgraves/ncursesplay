5f4b0ce0330d43fcc70fce87ef8ee5e63597841e
robgraves <matthewpage2003@yahoo.com>
2016-05-10 21:59:31 -0400

    made moving ball across screen

--- demo.c
+++ demo.c
@@ -1,20 +1,30 @@
-//demo.c
 
+//demo.c
+//Something something dark side...
 
 #include <ncurses.h>
 #include <unistd.h>
 
+#define DELAY 30000
+
 int main()
 {
+    int x = 0;
+    int y = 0;
 
-    initscr();          //Initialize the window
-    noecho();           //Don't echo any keypresses
-    curs_set(FALSE);    //Don't Display a cursor   
+    initscr();              //Initialize the window
+    noecho();               //Don't echo any keypresses
+    curs_set(FALSE);        //Don't display a cursor
 
-    mvprintw(0,0, "Hello, World!");
-    refresh();
+    while(1)
+    {
+        clear();            //clear screen of previously printed characters
+        mvprintw(y,x, "o"); //Print our "ball" at the current xy position
+        refresh();
 
-    sleep(1);
+        usleep(DELAY);      //Shorter delay between movements
+        x++;                //Advance the ball to the right   
+    }
 
-    endwin();   //Restore normal terminal behavior
+    endwin();               //Restore normal terminal behavior
 }
